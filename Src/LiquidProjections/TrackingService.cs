851f6553658cb0d43b785df801f3c809973846e9
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2016-07-07 13:00:41 +0200

    Added a simple implementation of the ITrackingService interface.

---
+++ Src/LiquidProjections/TrackingService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using eVision.FlowVision.Infrastructure.Common.Raven.Liquid;
+
+namespace LiquidProjections
+{
+    public class TrackingService
+    {
+        private readonly ITrackingStore store;
+
+        public TrackingService(ITrackingStore store)
+        {
+            this.store = store;
+        }
+
+        public async Task<string> GetLastCheckpoint(string projectorId)
+        {
+            string checkPoint = await store.LoadCheckpoint(projectorId);
+            return checkPoint ?? "";
+        }
+
+        public Task SaveCheckpoint(string projectorId, string checkpoint)
+        {
+            return store.SaveCheckpoint(projectorId, checkpoint);
+        }
+    }
+}
