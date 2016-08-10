d62e31507ae2c64d4cb1c8f82a997728af4cd7a3
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2016-08-10 11:30:05 +0200

    Updated to the 1.0 version of FluidCachign

--- Src/LiquidProjections.RavenDB/App_Packages/FluidCaching.Sources/GetKey.cs
+++
@@ -1,4 +0,0 @@
-namespace FluidCaching
-{
-    public delegate TKey GetKey<T, TKey>(T item) where T : class;
-}
--- Src/LiquidProjections.RavenDB/App_Packages/FluidCaching.Sources/GetNow.cs
+++
@@ -1,6 +0,0 @@
-using System;
-
-namespace FluidCaching
-{
-    public delegate DateTime GetNow();
-}
