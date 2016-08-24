7d49a67b945e7f8913a29ff4e7c7c1aeeaeef732
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2016-08-24 13:25:08 +0200

    The NES adapter would run in an infinite CPU-consuming loop if there's nothing to read.

---
+++ Tests/LiquidProjections.NEventStore.Specs/ChillExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Chill;
+
+namespace LiquidProjections.NEventStore.Specs
+{
+    internal static class ChillExtensions
+    {
+        public static void GivenAsync<T>(this GivenSubject<T> given, Func<Task> givenFuncAsync) where T : class
+        {
+            given.Given(() => Task.Run(givenFuncAsync).Wait());
+        }
+        public static void WhenAsync<T>(this GivenSubject<T> given, Func<Task> givenFuncAsync) where T : class
+        {
+            given.When(() => Task.Run(givenFuncAsync).Wait());
+        }
+    }
+}
