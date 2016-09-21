96745fcc2af3cfc300a463f6b4334fc3f5d9924e
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2016-09-21 11:18:19 +0200

    Refined the projection cache interface and removed the dependency on IHaveIdentity. (#29)

--- Src/LiquidProjections.RavenDB/IProjectionCache.cs
+++ Src/LiquidProjections.RavenDB/IProjectionCache.cs
@@ -3,9 +3,26 @@ using System.Threading.Tasks;
 
 namespace LiquidProjections.RavenDB
 {
-    public interface IProjectionCache<TProjection> where TProjection : IHaveIdentity
+    /// <summary>
+    /// Defines a write-through cache that the <see cref="RavenProjector{TProjection}"/> can use to avoid unnecessary loads
+    /// of the projection.
+    /// </summary>
+    public interface IProjectionCache<TProjection> 
     {
+        /// <summary>
+        /// Attempts to get the item identified by <paramref name="key"/> from the cache, or creates a new one. 
+        /// </summary>
+        /// <remarks>
+        /// This method must be safe in multi-threaded scenarios. So multiple concurrent requests for the same key must always 
+        /// return the same object.
+        /// </remarks>
         Task<TProjection> Get(string key, Func<Task<TProjection>> createProjection);
+
+        /// <summary>
+        /// Removes the item identified by <paramref name="key"/> from the cache.
+        /// </summary>
+        /// This method must be safe in multi-threaded scenarios and be idempotent. 
+        /// </summary>
         void Remove(string key);
     }
 }
