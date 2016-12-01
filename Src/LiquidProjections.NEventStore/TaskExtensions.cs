154383d0a98ddf3bd44a1081972dfb3bb7d4c5c4
danielmarbach <daniel.marbach@openplace.net>
2016-12-01 13:17:49 +0100

    Slightly improved version of WithWaitCancellation

--- Src/LiquidProjections.NEventStore/TaskExtensions.cs
+++ Src/LiquidProjections.NEventStore/TaskExtensions.cs
@@ -6,24 +6,39 @@ namespace LiquidProjections.NEventStore
 {
     internal static class TaskExtensions
     {
-        public static async Task<TResult> WithWaitCancellation<TResult>(this Task<TResult> task,
+        public static Task<TResult> WithWaitCancellation<TResult>(this Task<TResult> task,
             CancellationToken cancellationToken)
         {
-            using (var combined = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken))
+            var tcs = new TaskCompletionSource<TResult>();
+            var registration = cancellationToken.Register(s =>
             {
-                var delayTask = Task.Delay(Timeout.Infinite, combined.Token);
-                Task completedTask = await Task.WhenAny(task, delayTask);
-                if (completedTask == task)
+                var source = (TaskCompletionSource<TResult>) s;
+                source.TrySetCanceled();
+            }, tcs);
+
+            task.ContinueWith((t, s) =>
+            {
+                var tcsAndRegistration = (Tuple<TaskCompletionSource<TResult>, CancellationTokenRegistration>) s;
+
+                if (t.IsFaulted && t.Exception!= null)
                 {
-                    combined.Cancel();
-                    return await task;
+                    tcsAndRegistration.Item1.TrySetException(t.Exception.InnerException);
                 }
-                else
+
+                if (t.IsCanceled)
                 {
-                    cancellationToken.ThrowIfCancellationRequested();
-                    throw new InvalidOperationException("Infinite delay task completed.");
+                    tcsAndRegistration.Item1.TrySetCanceled();
                 }
-            }
+
+                if (t.IsCompleted)
+                {
+                    tcsAndRegistration.Item1.TrySetResult(t.Result);
+                }
+
+                tcsAndRegistration.Item2.Dispose();
+            }, Tuple.Create(tcs, registration), CancellationToken.None, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
+
+            return tcs.Task;
         }
     }
 }
