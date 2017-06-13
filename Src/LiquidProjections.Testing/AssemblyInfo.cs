f61c3a8d666d4413de1399df395ab90c527ec1a2
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2017-06-13 13:37:02 +0200

    The assembly versions were not correctly updated in the build. (#97)

---
+++ Src/LiquidProjections.Testing/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LiquidProjections.Testing")]
+[assembly: AssemblyDescription("Helper classes to support testing projects based on LiquidProjections.")]
+[assembly: AssemblyCopyright("Copyright Dennis Doomen 2016-2017")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0+Branch.master.Sha.6e68f840c3303bac97b10c567815a3bfef3184d7")] 
---
+++ Src/LiquidProjections/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LiquidProjections")]
+[assembly: AssemblyDescription("Provides basic building blocks for creating asynchronous projectors that subscribe to an event store.")]
+[assembly: AssemblyCopyright("Copyright Dennis Doomen 2016-2017")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0+Branch.master.Sha.6e68f840c3303bac97b10c567815a3bfef3184d7")] 
